f1b743343150f586a1f9aab9807c61e3eff20802
Angular0527 <Angular0527@outlook.com>
2023-04-26 16:51:33 +0800

    modified header component UI for responsive.

--- src/app/containers/default-layout/default-header/default-header.component.ts
+++ src/app/containers/default-layout/default-header/default-header.component.ts
@@ -2,20 +2,33 @@ import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 import { ClassToggleService, HeaderComponent } from '@coreui/angular';
+import {CONSTANT} from "../../../../constant/constant";
 
 @Component({
   selector: 'app-default-header',
   templateUrl: './default-header.component.html',
+  styleUrls: ['./default-header.component.sass']
 })
 export class DefaultHeaderComponent extends HeaderComponent {
 
   @Input() sidebarId: string = "sidebar";
 
-  public newMessages = new Array(4)
-  public newTasks = new Array(5)
-  public newNotifications = new Array(5)
-
   constructor(private classToggler: ClassToggleService) {
     super();
+  }
+
+  bsc_info = {
+    url: CONSTANT.HEADER_BSC_URL,
+    alt: CONSTANT.HEADER_BSC_ALT
+  }
+
+  wallet_info = {
+    url: CONSTANT.HEADER_WALLET_URL,
+    alt: CONSTANT.HEADER_WALLET_ALT
+  }
+
+  user_info = {
+    url: CONSTANT.HEADER_USER_URL,
+    alt: CONSTANT.HEADER_USER_ALT
   }
 }
